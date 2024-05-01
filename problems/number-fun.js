c799ab3a1b0b61b270a36ec9c22dac3333381f89
Mitchell Tsui <tsuimv@DESKTOP-CR6MCST.localdomain>
2024-05-01 06:00:29 -0400

    phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,10 +1,16 @@
 function returnsThree() {
-  // Your code here 
+  return 3
 }
 
 function reciprocal(n) {
-  // Your code here 
+  if (n < 1 || n > 1000000) {
+    throw new RangeError('Number is not between 1 to 1,000,000')
+  }
+
+  return 1/n 
 }
+
+console.log(reciprocal(1000))
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,33 @@
 // Your code here 
+const chai = require('chai')
+const expect = chai.expect
+const { returnsThree, reciprocal,  } = require(`../problems/number-fun`)
+
+describe('returnsThree', () => {
+    it('should return 3', () => {
+        expect(returnsThree()).to.equals(3)
+    })
+})
+
+describe('reciprocal', () => {
+    it('should return reciprocal', () => {
+        const num1 = 10
+        const num2 = 3
+        const num3 = 5
+
+        expect(reciprocal(num1)).to.equal(1/num1)
+        expect(reciprocal(num2)).to.equal(1/num2)
+        expect(reciprocal(num3)).to.equal(1/num3)
+    })
+    it('should intake ONLY numbers 1 between 1,000,000', () => {
+        const num4 = 100
+        // const num5 = 2000000
+
+        if ((num4 < 1) || (num4 > 1000000)) {
+            expect(reciprocal(num4)).to.throw(RangeError)
+        }
+        // if ((num5 < 1) || (num5 > 1000000)) {
+        //     expect(reciprocal(num5)).to.throw(RangeError)
+        // }
+    })
+})
