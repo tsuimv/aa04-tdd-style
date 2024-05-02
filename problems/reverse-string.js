953444adbe5fb025eb221c71f790d32ad44e0227
robweekes <weekes.robert@gmail.com>
2024-05-02 16:38:33 -0700

    Ph1 reverse-string and spec

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,36 @@
-module.exports = function reverseString(string) {
-  // Your code here 
+function reverseString(string) {
+  if(typeof string === 'number') {
+    throw new TypeError('thats a number, string arg must be a string')
+  }
+  if(Array.isArray(string)) {
+    throw new TypeError('thats an array, string arg must be a string')
+  }
+  if(typeof string === 'object') {
+    throw new TypeError('thats an object, string arg must be a string')
+  } else
+  if(typeof string !== 'string') {
+    throw new TypeError('string argument must be a string')
+  }
+
+
+    let result = '';
+    for(let i = string.length - 1; i >= 0; i--) {
+        let char = string[i];
+        result += char;
+        // console.log(result);
+      }
+      console.log(result);
+      return result;
 };
+
+// let bob = {name: Bob};   // says 'Bob is not defined'
+
+// reverseString(5);
+reverseString('fun');
+// reverseString([1,2,3]);
+// reverseString({});
+
+
+
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,50 @@
-// Your code here 
+const chai = require('chai');
+const expect = chai.expect;
+
+
+const reverseString = require('../problems/reverse-string');
+describe('reverseString', () => {
+    it('should return string reversed', () => {
+        expect(reverseString('fun')).to.equal('nuf');
+    })
+    it('should throw a TypeError if not a string', () => {
+        const number = 5;
+        const array = ["i", "am", "an", "array"];
+        const object = { i: "am", an: "object" };
+        const string = "i am a string";
+
+        if(typeof number === 'number') {
+            expect(() => reverseString(number)).to.throw(TypeError);
+        }
+        if(Array.isArray(array)) {
+            expect(() => reverseString(array)).to.throw(TypeError);
+        }
+        if(typeof object === 'object') {
+            expect(() => reverseString(object)).to.throw(TypeError);
+        } else
+        if(typeof string !== 'string') {
+            expect(() => reverseString(string)).to.throw(TypeError);
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+// template from AA04-unit testing
+// it("should throw an error if num is not type of Number", () => {
+//     const string = "i am a string";
+//     const object = { i: "am", an: "object" };
+//     const array = ["i", "am", "an", "array"];
+
+//     expect(() => isOdd(string)).to.throw(Error);
+//     expect(() => isOdd(object)).to.throw(Error);
+//     expect(() => isOdd(array)).to.throw(Error);
+//   });
+// });
