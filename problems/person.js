5fc3bc34674cfa5fe52709578e7cacc733aa9478
robweekes <weekes.robert@gmail.com>
2024-05-02 18:15:24 -0700

    working on Phase 3

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,22 @@
 class Person {
-  // Your code here 
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // INSTANCE METHODS
+    sayHello() {
+        return `Hello, ${this.name}`;
+    }
+
+    visit(person2) {
+        return `${this.name} visited ${person2}`
+    }
+
 }
+
+// const coolPerson = new Person('Mai', 32);
+// console.log(coolPerson.visit('Erin')); // 'Mai visited Erin'
+
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,28 @@
-// Your code here 
+const chai = require('chai');
+const expect = chai.expect;
+
+
+const Person = require('../problems/person');
+
+describe('constructor', () => {
+    it('should take in a name and an age', () => {
+        expect(() => constructor.name).to.exist;
+        expect(() => this.name).to.equal(this.name);
+        expect(() => constructor.age).to.exist;
+        expect(() => this.age).to.equal(this.age);
+    })
+});
+
+describe('sayHello()', () => {
+    it("should return a string of the Person instance's name and a greeting message", () => {
+        expect(() => sayHello()).to.equal(`Hello, ${this.name}`);
+    })
+});
+
+describe('visit(otherPerson)', () => {
+    it('should return a string stating that this instance visited the passed-in person', () => {
+        let person1 = new Person('Mai');
+        let person2 = new Person('Erin');
+        expect(() => visit(person2.name)).to.equal(`${person1.name} visited ${person2.name}`)
+    })
+})
